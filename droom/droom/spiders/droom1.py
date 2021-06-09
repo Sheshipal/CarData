79a1616ca2648154bbcde700c9a6cb5af2146e27
sheshipal <sheshipalkarivedula@gmail.com>
2021-06-09 08:23:54 +0530

    added xpaths

--- droom/droom/spiders/droom1.py
+++ droom/droom/spiders/droom1.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy_splash import SplashRequest
+from collections import defaultdict
 
 class Droom1Spider(scrapy.Spider):
     name = 'droom1'
@@ -29,6 +30,56 @@ class Droom1Spider(scrapy.Spider):
         print(f"links : {links}")
 
     def parse_details(self, response):
-        data = response.xpath("//div[contains(@id, 'description')]").extract()
+        data = defaultdict(lambda:None)
 
-        print(data)
+        # extract name
+        try:
+            name = response.xpath("//div[contains(@class,'listing-card')]/h1/text()").extract_first()
+            data['name'] = name
+        except():
+            name = None
+
+        # extract price
+        try:
+            price = response.xpath("//div[text()='Selling Price']/parent::node()/div[not(contains(text(), 'Selling Price'))]/text()").extract_first()
+            data['price'] = price
+        except():
+            price = None
+
+        try:
+            # extract summary block
+            summary = response.xpath("//div[contains(@class ,'product-summery')]")
+
+            # get div blocks in summary
+            blocks = summary[0].xpath(".//div/div") 
+            product_summary = blocks[0]
+            tech_specs = blocks[1]
+            
+            # get li elements in block0
+            product_details = product_summary.xpath("//div/h2[text()= 'Product Summary']/parent::node()/div/ul/li/div/text()").extract()
+            
+            # get li elements in block1
+            tech_specifications = tech_specs.xpath("//div/h2[contains(text(),'Tech Specs')]/parent::node()/div/ul/li/div/text()").extract()
+            specifications = []
+            for specification in tech_specifications:
+                data = specification.strip()
+                if len(data) > 0:
+                    specifications.append(data)
+
+            # seperete data
+            data['owner'] = product_details[0]
+            data['driven km'] = product_details[1]
+            data['transmission'] = product_details[2]
+            data['fuel'] = product_details[3]
+            data['body type'] = product_details[4]
+            data['mileage'] = specifications[0]
+            data['engine cc'] = specifications[1]
+            data['max power bhp'] = specifications[2]
+            data['wheel size inches'] = specifications[3]
+            data['no of seats'] = specifications[4]
+        except:
+            pass
+
+        print()
+
+        yield {"data" : data}
